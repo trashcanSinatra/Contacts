e57dd327e084e2463f5d2ff52e5b7eafd5d91cd4
Aaron Vogt <atvogt@localhost.localdomain>
2015-12-09 22:39:43 -0500

    Added NewCall View Div to index.php

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 
 		<!-- <link rel="stylesheet" type="text/css" href="css/hover-min.css" /> -->
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/newCall.css" />
 		<link rel="stylesheet" type="text/css" href="css/slide.css" />
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 		<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
     	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     	<link href='http://fonts.googleapis.com/css?family=Cedarville+Cursive'rel='stylesheet'type='text/css'>
@@ -22,6 +23,7 @@
 
 	   <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 	   <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	</head>
 <!-- SLIDE MENUS -->
 
@@ -29,18 +31,18 @@
 	<div id="container">
 	  <div id="leftPane">
 		<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+			<a id="addyBookLink" href="#">Address Book</a>
 			<a href="#">Wholesalers</a>
 			<a href="#">Contractors</a>
 			<a href="#">Engineers</a>
-			<a href="#">Principals</a>
 			<a href="#">New Contact</a>
 			<a href="#">Sales Calls</a>
-			<a href="#">New Call</a>
+			<a id="newCallLnk" href="#">New Call</a>
 		</nav>
 	</div>
 	<div id="rightPane">
 		<div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
-		<h3><input style="padding-right: auto;padding-left: auto;width: 100%;"type="text" id="mainSearch" placeholder="Search Contacts.." /></h3>
+		<h3>Contacts</h3>
 		</div>
 	</div>
 
@@ -49,7 +51,7 @@
 	<div id="headerBackground" class="header-cont">
 	<header>
 		<ul>
-		  <div id="pc501"><span style="color:#FE8B05;">pc</span>501</div>
+		  <div id="pc501"><span style="color:#FE8B05;">rep</span>Lynk</div>
 
 		   <div id="headerBtnLeft">
 		   <button id="showLeft" style="backgroundColor:#76CA8F;"
@@ -68,10 +70,6 @@
 	</header>
    </div>
 
-	<!-- <div id="testRegion">
-	<button id="testBtn" style="height:50px;width:100px;margin-top:150px;
-										 margin-right:50px;float:right;">AJAX Test</button>
-	</div> -->
 
 	<div id="mainContent">
 		<div id="filterToggle">
@@ -89,13 +87,21 @@
 		</div>
 
 		<div id="results"></div>
+	</div> <!-- End Main Content -->
+
+
+	<div id="view_newCall">
+		<h2 id="newCallHeader"> New Sales Call </h2>
+		<p>Date: <input type="text" id="datepicker"></p>
 	</div>
+
 
 	</div>  <!-- End Container -->
 
 
 <script src="js/vendor/classie.js"></script>
 <script src="js/components/menus.js"></script>
+<script src="js/components/menuNav.js"></script>
 <script src="js/templates/phoneBookTemplate.js"></script>
 <script src="js/services/phoneBookService.js"></script>
 <script src="js/app.js"></script>
